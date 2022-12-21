aa8292b2ce050f22ceecd3a42920dcb9f8043b20
Andrew Jonatan Damanik <andrewdamanik23@gmail.com>
2022-12-21 10:33:44 +0700

    Add CalculatorViewModel.kt to accept user events like when clicking number button, operation button, etc and the view model will accept that events and map it to new state.

---
+++ app/src/main/java/com/andrew/calculator/CalculatorViewModel.kt
@@ -0,0 +1,11 @@
+package com.andrew.calculator
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class CalculatorViewModel: ViewModel() {
+    var state by mutableStateOf(CalculatorState())
+    private set
+}
